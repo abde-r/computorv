3b75b51e8b129936ab8fc08f904f1665ba2d70e4
stronkexe <elasri9999@gmail.com>
2024-07-03 00:04:06 +0100

    initial commit.

---
+++ main.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    println!("{}", args.len());
+    for arg in env::args() {
+        println!("{arg}");
+    }
+}
